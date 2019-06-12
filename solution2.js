0f31c9bf83d3eb7328a1e1ee303f1481e5fb98c6
Haposan12 <38253700+Haposan12@users.noreply.github.com>
2019-06-12 09:41:16 +0700

    Update solution2.js

--- solution2.js
+++ solution2.js
@@ -1,39 +1,40 @@
-function solution(N,users){
+function solution(N, users){
+
   var answer = [];
-  var hasil;
-  var pembilang = 0;
-  var penyebut = 0;
-  
-  for(i=1;i<=N;i++){
-    for(j in users){
-      if(users[j] == i){
-        pembilang++;
-      }
-      if(users[j] >= i){
-        penyebut++;
-      }
+  users.sort((a,b) => a-b);
+  var len = users.length;
+  var count = 0;
+  var i = 0;
+
+  while(i < len){
+    if(users[i] !== users[i+1] && users[i]<=N){
+      count++;
+      var x = (len-1)-i + count;
+      answer.push([users[i], count/x]);
+      count = 0;
+    }else{
+      count++;
     }
-    hasil = pembilang/penyebut;
-    answer.push([i, hasil]);
-    pembilang = 0;
-    penyebut = 0;
-    
+    i++;
   }
-  //sort with descending
+
+  for(var j=1;j<=N;j++){
+    if(!users.includes(j)){
+      answer.push([j , 0]);
+    }
+  }
+
   answer.sort((a, b) => b[1] - a[1]);
 
-  //looping to take the only stage 
   for(var i in answer){
     answer[i] = answer[i][0]
   }
-  
+
   return answer;
 }
 
-
 //testing
+var n = 5;
 var user = [2,1,2,6,2,4,3,3];
-
-console.log(solution(5,user)); 
-
-//answer : [3,4,2,1,5]
+//1,2,2,2,3,3,4,6
+console.log(solution(n,user));
